2734507549ad6ef6c8618ba00b8fb96dd3b698f7
Tim Lingard <tklingard@gmail.com>
2017-09-10 13:24:25 +0100

    Fixing babel, gulp and linting

---
+++ gulpfile.babel.js
@@ -0,0 +1,27 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-console */
+import gulp from 'gulp';
+import eslint from 'gulp-eslint';
+
+// define where our paths are
+const paths = {
+  allSrcJs: 'src/**/*.js?(x)', // all js(x) files
+  srcJs: 'src/**/*.js?(x)', // server js(x) files
+  serverEntryPoint: 'src/index.js', // client connection file
+  gulpFile: 'gulpfile.babel.js', // gulp configuration file
+  libDir: 'lib',
+};
+
+// linting task
+gulp.task('lint', () =>
+  gulp.src([
+    paths.allSrcJs,
+    paths.gulpFile,
+  ])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError()),
+);
+
+
+gulp.task('default', ['lint']);
